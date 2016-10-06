ce44d0b1986cdb0b2f7692aa9451cbd570831a91
Kirill <c.derche@me.com>
2016-10-06 18:59:59 +0200

    Update migra

--- db/migrate/20161004202031_fixaddressmigration.rb
+++ db/migrate/20161004202031_fixaddressmigration.rb
@@ -1,6 +1,6 @@
 class Fixaddressmigration < ActiveRecord::Migration[5.0]
   def change
-    remove_index :addresses, name: :index_addresses_on_cart_id_id
-    remove_index :addresses, name: :index_addresses_on_user_id_id
+    # remove_index :addresses, name: :index_addresses_on_cart_id_id
+    # remove_index :addresses, name: :index_addresses_on_user_id_id
   end
 end
