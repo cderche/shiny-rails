189d833013220f38a729368b2c8363d203968d9f
Kirill <c.derche@me.com>
2016-10-06 18:50:51 +0200

    Updated migration

--- db/migrate/20161004163120_create_carts.rb
+++ db/migrate/20161004163120_create_carts.rb
@@ -1,4 +1,4 @@
-class CreateCarts < ActiveRecord::Migration[5.0]
+class CreateCartsAndAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :carts do |t|
       t.string   :frequency
@@ -7,6 +7,14 @@ class CreateCarts < ActiveRecord::Migration[5.0]
       t.integer  :duration
       t.boolean  :ironing
       t.boolean  :pets
+      t.timestamps
+    end
+    create_table :addresses do |t|
+      t.string   :firstname
+      t.string   :lastname
+      t.string   :city
+      t.string   :postcode
+      t.string   :phone
       t.timestamps
     end
   end
