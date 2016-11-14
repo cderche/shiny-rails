0ab35a62a4a1129f033e00a3b3edca0a239d320f
Kirill <c.derche@me.com>
2016-11-14 18:29:32 +0100

    Fixed migration

--- db/migrate/20161006095058_modify_addresses.rb
+++ db/migrate/20161006095058_modify_addresses.rb
@@ -1,7 +1,7 @@
 class ModifyAddresses < ActiveRecord::Migration[5.0]
   def change
-    remove_column :addresses, :address1
-    remove_column :addresses, :address2
+    # remove_column :addresses, :address1
+    # remove_column :addresses, :address2
     add_column    :addresses, :street,    :string
     add_column    :addresses, :block,     :string
     add_column    :addresses, :house,     :string
