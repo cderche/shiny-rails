4601945f2551af2cda3f765f34a790f8f3131894
Kirill <c.derche@me.com>
2016-11-26 02:51:01 +0300

    remove ref with index

--- db/migrate/20161124171539_drop_v1_tables.rb
+++ db/migrate/20161124171539_drop_v1_tables.rb
@@ -2,9 +2,9 @@ class DropV1Tables < ActiveRecord::Migration[5.0]
   def change
     drop_table :carts
     drop_table :admins
-    remove_reference :addresses, :order
-    remove_reference :notifications, :order
-    drop_table :orders, force: true
+    remove_reference :addresses, :order, index: true
+    remove_reference :notifications, :order, index: true
+    drop_table :orders
     drop_table :promos
   end
 end
