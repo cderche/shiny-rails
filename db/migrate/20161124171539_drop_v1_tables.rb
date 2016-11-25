96efac3c20b05833ae86019b1d876b59259dd6d7
Kirill <c.derche@me.com>
2016-11-26 02:37:20 +0300

    Remove order ref

--- db/migrate/20161124171539_drop_v1_tables.rb
+++ db/migrate/20161124171539_drop_v1_tables.rb
@@ -2,6 +2,8 @@ class DropV1Tables < ActiveRecord::Migration[5.0]
   def change
     drop_table :carts
     drop_table :admins
+    remove_reference :addresses, :order
+    remove_reference :notifications, :order
     drop_table :orders
     drop_table :promos
   end
