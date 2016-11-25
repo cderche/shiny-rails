fd4ea2d81eb5257805954c70b692ee2502fa3c03
Kirill <c.derche@me.com>
2016-11-26 02:45:30 +0300

    drop table force

--- db/migrate/20161124171539_drop_v1_tables.rb
+++ db/migrate/20161124171539_drop_v1_tables.rb
@@ -4,7 +4,7 @@ class DropV1Tables < ActiveRecord::Migration[5.0]
     drop_table :admins
     remove_reference :addresses, :order
     remove_reference :notifications, :order
-    drop_table :orders
+    drop_table :orders, force: true
     drop_table :promos
   end
 end
