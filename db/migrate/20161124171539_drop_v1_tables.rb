f8a9e7757de7f444a5cb423fa6b56e27f9a9629b
Kirill <c.derche@me.com>
2016-11-26 03:18:41 +0300

    again

--- db/migrate/20161124171539_drop_v1_tables.rb
+++ db/migrate/20161124171539_drop_v1_tables.rb
@@ -3,7 +3,7 @@ class DropV1Tables < ActiveRecord::Migration[5.0]
     remove_reference :addresses, :cart, index: true
     remove_reference :orders, :cart, index: true
     remove_reference :notifications, :order, index: true
-    remove_reference :addresses, :order, index: true
+    # remove_reference :addresses, :order, index: true
     # remove_reference :address, :order, index: true
     drop_table :carts
     drop_table :admins
