6c5c955cd6cdaad92bcc60d3a59f9943ed403028
Kirill <c.derche@me.com>
2017-01-16 16:15:30 +0300

    Modal created when 'Book' pressed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   def fullname
     "#{self.firstname} #{self.lastname}"
