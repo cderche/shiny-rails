647cbdad4e7b743a42febd84e750cffab6b350bf
Kirill <c.derche@me.com>
2017-01-23 21:54:43 +0300

    Updated migration.

--- db/migrate/20161031154044_change_promo_object.rb
+++ db/migrate/20161031154044_change_promo_object.rb
@@ -1,7 +1,7 @@
 class ChangePromoObject < ActiveRecord::Migration[5.0]
   def change
-    enable_extension 'hstore' unless extension_enabled?('hstore')
-    remove_column :promos, :value
-    add_column :promos, :values, :hstore
+    #enable_extension 'hstore' unless extension_enabled?('hstore')
+    #remove_column :promos, :value
+    #add_column :promos, :values, :hstore
   end
 end
