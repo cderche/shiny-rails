827f5c7f064b98d58a517f7e71347c6ad2ef8158
Kirill <c.derche@me.com>
2017-01-24 20:02:47 +0300

    AddDeviseCols.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124163923) do
+ActiveRecord::Schema.define(version: 20170124165904) do
 
   create_table "addons", force: :cascade do |t|
     t.integer  "booking_id"
