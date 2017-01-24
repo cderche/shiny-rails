b802b4fc8b2f8a19ede112580f49743f5ccd7b15
Kirill <c.derche@me.com>
2017-01-25 00:45:36 +0300

    Fix NotificationService.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,8 @@ class NotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-    puts notification_params.to_json
+    # json = notification_params.to_json
+    # puts "Params: #{notification_params.to_json}"
     if NotificationService.handle(notification_params)
       head :ok
     else
--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,16 +1,22 @@
 class NotificationService
 
   def self.handle(data)
-    puts data
+    # puts "JSON: #{data}"
+    puts "Notification: #{data[:Notification]}"
     case data[:Notification]
-    when 'CustomerAddSuccess'
-      complete_booking(data)
+    when "CustomerAddSuccess", "CustomerPaySuccess"
+        complete_booking(data)
+        return true
+      else
+        return false
     end
+
   end
 
   private
 
   def self.complete_booking(data)
+    puts "Complete Booking"
     # Find the booking
     @booking = Booking.find_by(order_token: data[:OrderId])
     # Add CardId as card_token
