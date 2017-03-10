28e8821cd85fe27930241c2df26571538b08cf32
Kirill <c.derche@me.com>
2017-03-10 15:27:30 +0300

    back to regular terms and questions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,18 +5,19 @@ class HomeController < ApplicationController
   end
 
   def terms
-    if I18n.locale == :ru
-      redirect_to 'https://support.getshiny.ru/hc/ru/articles/115000783729'
-    else
-      redirect_to 'https://support.getshiny.ru/hc/en-us/articles/115000783729'
-    end
+    # if I18n.locale == :ru
+    #   # redirect_to 'https://support.getshiny.ru/hc/ru/articles/115000783729'
+    # else
+    #   # redirect_to 'https://support.getshiny.ru/hc/en-us/articles/115000783729'
+    # end
   end
 
   def questions
-    if I18n.locale == :ru
-      redirect_to 'https://support.getshiny.ru/hc/ru/sections/115000222769'
-    else
-      redirect_to 'https://support.getshiny.ru/hc/en-us/sections/115000222769'
-    end
+    # if I18n.locale == :ru
+    #   # redirect_to 'https://support.getshiny.ru/hc/ru/sections/115000222769'
+    # else
+    #   # redirect_to 'https://support.getshiny.ru/hc/en-us/sections/115000222769'
+    #   redirect_to 'https://getshiny.zendesk.com/hc/en-us/categories/115000106365-FAQ'
+    # end
   end
 end
