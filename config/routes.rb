ac522207b7f26f408af0a0d1f9af87db92b36a49
Kirill <c.derche@me.com>
2017-04-14 15:12:44 +0300

    root route to new page

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
   get '/terms', to: 'home#terms'
   get '/questions', to: 'home#questions'
 
-  root to: "home#index"
-  get '/beta', to: "home#home"
+  root to: "home#home"
+  # get '/beta', to: "home#home"
 
   get '/book', to: 'bookings#new'
   get '/bookings', to: 'bookings#new'
