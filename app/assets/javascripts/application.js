fefd8bd2cdd130eb7980470ab336b8e4ff8e212d
Kirill <c.derche@me.com>
2017-04-14 14:19:10 +0300

    remove =

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,36 +12,36 @@
 //
 
 // BEGIN VENDOR JS FOR PAGES (plus add in jquery.turbolinks)
-//= require pages-plugins/pace/pace.min
-//= require pages-plugins/jquery/jquery-1.11.1.min
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require pages-plugins/modernizr.custom
-//= require pages-plugins/jquery-ui/jquery-ui.min
-//= require pages-plugins/bootstrapv3/js/bootstrap.min
-//= require pages-plugins/jquery/jquery-easy
-//= require pages-plugins/jquery-unveil/jquery.unveil.min
-//= require pages-plugins/jquery-bez/jquery.bez.min
-//= require pages-plugins/jquery-ios-list/jquery.ioslist.min
-//= require pages-plugins/imagesloaded/imagesloaded.pkgd.min
-//= require pages-plugins/jquery-actual/jquery.actual.min
-//= require pages-plugins/jquery-scrollbar/jquery.scrollbar.min
-//= require pages-plugins/bootstrap-datepicker/js/bootstrap-datepicker
-//= require pages-plugins/bootstrap-timepicker/bootstrap-timepicker.min
-//= require pages-plugins/bootstrap-select2/select2.min
-//= require pages-plugins/moment/moment.min
-//= require pages-plugins/jquery-datatable/media/js/jquery.dataTables.min
-//= require pages-plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min
-//= require pages-plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap
-//= require pages-plugins/datatables-responsive/js/datatables.responsive
-//= require pages-plugins/datatables-responsive/js/lodash.min
+// require pages-plugins/pace/pace.min
+// require pages-plugins/jquery/jquery-1.11.1.min
+// require jquery.turbolinks
+// require jquery_ujs
+// require pages-plugins/modernizr.custom
+// require pages-plugins/jquery-ui/jquery-ui.min
+// require pages-plugins/bootstrapv3/js/bootstrap.min
+// require pages-plugins/jquery/jquery-easy
+// require pages-plugins/jquery-unveil/jquery.unveil.min
+// require pages-plugins/jquery-bez/jquery.bez.min
+// require pages-plugins/jquery-ios-list/jquery.ioslist.min
+// require pages-plugins/imagesloaded/imagesloaded.pkgd.min
+// require pages-plugins/jquery-actual/jquery.actual.min
+// require pages-plugins/jquery-scrollbar/jquery.scrollbar.min
+// require pages-plugins/bootstrap-datepicker/js/bootstrap-datepicker
+// require pages-plugins/bootstrap-timepicker/bootstrap-timepicker.min
+// require pages-plugins/bootstrap-select2/select2.min
+// require pages-plugins/moment/moment.min
+// require pages-plugins/jquery-datatable/media/js/jquery.dataTables.min
+// require pages-plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min
+// require pages-plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap
+// require pages-plugins/datatables-responsive/js/datatables.responsive
+// require pages-plugins/datatables-responsive/js/lodash.min
 
 // BEGIN CORE TEMPLATE JS FOR PAGES
-// = require pages-core/js/pages.min
+//  require pages-core/js/pages.min
 
 // BEGIN SITE SCRIPTS
-//= require ./pages
+// require ./pages
 
 //  I prefer to list scripts in a specific order, so I comment out require_tree .
-//= require_tree .
-//= require turbolinks
+// require_tree .
+// require turbolinks
