9b94e49a283fe47d5b53998b0aa0586bdf4514a5
Kirill <c.derche@me.com>
2017-04-14 14:33:44 +0300

    js fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,17 +24,17 @@
 //= require pages-plugins/jquery-bez/jquery.bez.min
 //= require pages-plugins/jquery-ios-list/jquery.ioslist.min
 //= require pages-plugins/imagesloaded/imagesloaded.pkgd.min
-// require pages-plugins/jquery-actual/jquery.actual.min
-// require pages-plugins/jquery-scrollbar/jquery.scrollbar.min
-// require pages-plugins/bootstrap-datepicker/js/bootstrap-datepicker
-// require pages-plugins/bootstrap-timepicker/bootstrap-timepicker.min
-// require pages-plugins/bootstrap-select2/select2.min
-// require pages-plugins/moment/moment.min
-// require pages-plugins/jquery-datatable/media/js/jquery.dataTables.min
-// require pages-plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min
-// require pages-plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap
-// require pages-plugins/datatables-responsive/js/datatables.responsive
-// require pages-plugins/datatables-responsive/js/lodash.min
+//= require pages-plugins/jquery-actual/jquery.actual.min
+//= require pages-plugins/jquery-scrollbar/jquery.scrollbar.min
+//= require pages-plugins/bootstrap-datepicker/js/bootstrap-datepicker
+//= require pages-plugins/bootstrap-timepicker/bootstrap-timepicker.min
+//= require pages-plugins/bootstrap-select2/select2.min
+//= require pages-plugins/moment/moment.min
+//= require pages-plugins/jquery-datatable/media/js/jquery.dataTables.min
+//= require pages-plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min
+//= require pages-plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap
+//= require pages-plugins/datatables-responsive/js/datatables.responsive
+//= require pages-plugins/datatables-responsive/js/lodash.min
 
 // BEGIN CORE TEMPLATE JS FOR PAGES
 //  require pages-core/js/pages.min
