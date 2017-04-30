25228a9a9f018cb715793d431926aedac2107798
Kirill <c.derche@me.com>
2017-04-30 18:07:07 +0300

    Closes #131

---
+++ app/helpers/login_helper.rb
@@ -0,0 +1,21 @@
+module LoginHelper
+
+  def login_button
+    if user_signed_in?
+      link_to t('menu.order') , clean_path  , class: 'btn btn-sm btn-bordered btn-black block-title hidden-sm hidden-xs'
+    else
+      link_to t('menu.order') , '#' , class: 'btn btn-sm btn-bordered btn-black block-title hidden-sm hidden-xs'  , data: { toggle: 'modal' , target: '#modalAuth' }
+    end
+  end
+
+  def login_link
+    if user_signed_in?
+      link_to t('menu.order') , clean_path  , class: 'link'
+    else
+      link_to t('menu.order') , '#' , class: 'link'  , data: { toggle: 'modal' , target: '#modalAuth' }
+    end
+  end
+end
+
+
+# <a class="btn btn-sm btn-bordered btn-black block-title fs-12 hidden-sm hidden-xs" href="#" data-toggle="modal" data-target="#modalAuth" data-text="<%= t('book_a_clean') %>"><%= t('book_a_clean') %></a>
