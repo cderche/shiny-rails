0e21177e9c7496d7c4da40d64b7b24bd3ee38c3a
Kirill <c.derche@me.com>
2017-08-01 11:51:23 +0300

    fixed status color

--- app/helpers/admin/invoices_helper.rb
+++ app/helpers/admin/invoices_helper.rb
@@ -1,12 +1,12 @@
 module Admin::InvoicesHelper
 
   def invoice_status_label(status)
-    content_tag :span, status, class: "label label-#{color(status)}"
+    content_tag :span, status, class: "label label-#{invoice_color(status)}"
   end
 
   private
 
-  def color(status)
+  def invoice_color(status)
     color = 'default'
     case status.to_sym
     when :draft
