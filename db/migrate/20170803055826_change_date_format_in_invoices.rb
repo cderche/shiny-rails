2cad42ddd23dfb888d486d3b909bbe6e1cf7b427
Kirill <c.derche@me.com>
2017-08-03 14:18:57 +0300

    fix migration

--- db/migrate/20170803055826_change_date_format_in_invoices.rb
+++ db/migrate/20170803055826_change_date_format_in_invoices.rb
@@ -1,20 +1,20 @@
 class ChangeDateFormatInInvoices < ActiveRecord::Migration[5.0]
   def up
-    change_column :invoices, :date, :date
-    # change_column :invoices, :date, 'date USING date::date'
-    Invoice.all.each do |i|
-      if i.date.kind_of? String
-        i.update(date: Date.strptime(i.date, "%d/%m/%Y"))
-      end
-    end
+    # change_column :invoices, :date, :date
+    change_column :invoices, :date, 'date USING date::date'
+    # Invoice.all.each do |i|
+    #   if i.date.kind_of? String
+    #     i.update(date: Date.strptime(i.date, "%d/%m/%Y"))
+    #   end
+    # end
   end
 
   def down
     change_column :invoices, :date, :string
-    Invoice.all.each do |i|
-      if i.date.kind_of? Date
-        i.update(date: i.date.strftime("%d/%m/%Y"))
-      end
-    end
+    # Invoice.all.each do |i|
+    #   if i.date.kind_of? Date
+    #     i.update(date: i.date.strftime("%d/%m/%Y"))
+    #   end
+    # end
   end
 end
